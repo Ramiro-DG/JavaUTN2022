6d6438d01ee491c9a41d13e575897407e680b671
Ramiro-DG <71228261+Ramiro-DG@users.noreply.github.com>
2022-08-18 18:42:31 -0300

    Add files via upload

---
+++ ParcialJava2021/src/ej1/Equipment.java
@@ -0,0 +1,20 @@
+package ej1;
+
+public class Equipment {
+	private String type;
+	private Double cost;
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Double getCost() {
+		return cost;
+	}
+	public void setCost(Double cost) {
+		this.cost = cost;
+	}
+}
